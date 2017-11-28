2c6a1d250465d0aa0a95e4c521585f09d83f8362
Greepex <15281148@bjtu.edu.cn>
2017-11-29 00:22:43 +0800

    Update word2voice.py

--- voice/word2voice.py
+++ voice/word2voice.py
@@ -4,22 +4,36 @@
 使用百度语音合成将文字转化为mp3
 '''
 import urllib2
-import json
 import sys
+import io
+import wave
+from pydub import AudioSegment
+import audio
 reload(sys)
 sys.setdefaultencoding('utf-8')
-class word2voice(object):
-    appkey = u''#百度appkey
-    secretkey = u''#百度secretkey
-    appid = u''#百度appid
-    tokenurl=u'https://openapi.baidu.com/oauth/2.0/token?grant_type=client_credentials&client_id=%s&client_secret=%s'%(appkey, secretkey)
-    voiceurl = u'http://tsn.baidu.com/text2audio?lan=zh&ctp=1&cuid=%s&tok=%s&tex=%s&vol=9'
-    def gettoken(self):
-        re_dict = json.loads(urllib2.urlopen(self.tokenurl).read())
-        return re_dict['access_token']
+from control.settings import BaiduVoiceFather
+class word2voice(BaiduVoiceFather):
     #调用getvoice可将语音文件放在项目路径下
     def getvoice(self,word):
         url = self.voiceurl%(self.appid,self.gettoken(),word)
-        with open('temp.mp3','wb') as f:
-            f.write(urllib2.urlopen(url).read())
+        self.mp32wav(urllib2.urlopen(url).read())
+
+    def mp32wav(self, data):
+        aud = io.BytesIO(data)
+        sound=AudioSegment.from_file(aud,format='mp3')
+        raw_data = sound._data
+        #写入到文件，验证结果是否正确。
+        l=len(raw_data)
+        f=wave.open("temp.wav",'wb')
+        f.setnchannels(1)
+        f.setsampwidth(2)
+        f.setframerate(16000)
+        f.setnframes(l)
+        f.writeframes(raw_data)
+        f.close()
+
+temp = word2voice()
+def playword(word):
+    temp.getvoice(word)
+    audio.playaudio()
             
