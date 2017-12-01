ddb5cad1df529a3bf2ee184cbab2332defa4a1b1
Greepex <15281148@bjtu.edu.cn>
2017-12-01 15:34:04 +0800

    Update audio.py

--- voice/audio.py
+++ voice/audio.py
@@ -7,6 +7,9 @@ import sys
 
 # 定义数据流块
 CHUNK = 1024
+CHANNELS = 2
+RATE = 44100
+RECORD_SECONDS = 5
 def playaudio():
     wf = wave.open(r'temp.wav', 'rb')
     p = pyaudio.PyAudio()
@@ -26,3 +29,28 @@ def playaudio():
     stream.close()
     # 关闭 PyAudio
     p.terminate()
+
+def getvoice():
+    FORMAT = pyaudio.paInt16
+    WAVE_OUTPUT_FILENAME = "output.wav"
+    p = pyaudio.PyAudio()
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+    print("* recording")
+    frames = []
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+    print("* done recording")
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
