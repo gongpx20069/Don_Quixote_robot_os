2be9d9adefd30841f4246d527f35e2a5e8254561
Greepex <15281148@bjtu.edu.cn>
2017-12-14 23:40:29 +0800

    Update word2voice.py

--- voice/word2voice.py
+++ voice/word2voice.py
@@ -9,6 +9,7 @@ import io
 import wave
 from pydub import AudioSegment
 import audio
+import re
 reload(sys)
 sys.setdefaultencoding('utf-8')
 from control.settings import BaiduVoiceFather
@@ -16,15 +17,15 @@ class word2voice(BaiduVoiceFather):
     #调用getvoice可将语音文件放在项目路径下
     def getvoice(self,word):
         url = self.voiceurl%(self.appid,self.gettoken(),word)
-        self.mp32wav(urllib2.urlopen(url).read())
+        self.mp32wav(data = urllib2.urlopen(url).read(),filename = 'temp.wav')
 
-    def mp32wav(self, data):
+    def mp32wav(self, data,filename):
         aud = io.BytesIO(data)
         sound=AudioSegment.from_file(aud,format='mp3')
         raw_data = sound._data
         #写入到文件，验证结果是否正确。
         l=len(raw_data)
-        f=wave.open("temp.wav",'wb')
+        f=wave.open(filename,'wb')
         f.setnchannels(1)
         f.setsampwidth(2)
         f.setframerate(16000)
@@ -34,6 +35,6 @@ class word2voice(BaiduVoiceFather):
 
 temp = word2voice()
 def playword(word):
+    word = re.sub('\s', '', word)
     temp.getvoice(word)
     audio.playaudio()
-            
